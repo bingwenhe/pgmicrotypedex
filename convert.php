f22e422c49f60ad20e71e767d6b7d2b03e096a20
Frédéric Potvin <frederic.potvin@savoirfairelinux.com>
2016-08-17 12:40:27 -0400

    Initial commit with useful files

---
+++ convert.php
@@ -0,0 +1,60 @@
+<?php # Minimum requirements: PHP7
+
+function main() {
+    $dex['json'] = file_get_contents('pgdex.json');
+    $dex['array'] = json_decode($dex['json'], $array = true);
+
+    foreach($dex['array'] as $mon) {
+        if(!isset($mon['Next evolution(s)'])) {
+            $name = $mon['Name'];
+            $types = shortenTypes($mon['Types']);
+            $fa = getAttTypes($mon['Fast Attack(s)']);
+            $ca = getAttTypes($mon['Special Attack(s)']);
+
+            $mdex[] = "$name ($types)$fa|$ca";
+        }
+    }
+
+    sort($mdex, SORT_STRING);
+    foreach($mdex as $entry) echo $entry."\n";
+}
+
+function getAttTypes($attacks) {
+    foreach($attacks as $key => $value)
+        $attacks[$key] = shortType($value['Type']??'-');
+
+    return implode($attacks);
+}
+
+function shortenTypes($types) {
+    foreach($types as $key => $value)
+        $types[$key] = shortType($value);
+
+    return implode($types);
+}
+
+function shortType($type) {
+    switch($type) {
+        case 'Normal': return 'N';
+        case 'Fighting': return 'T';
+        case 'Flying': return 'Y';
+        case 'Poison': return 'P';
+        case 'Ground': return 'G';
+        case 'Rock': return 'R';
+        case 'Bug': return 'B';
+        case 'Ghost': return 'O';
+        case 'Steel': return 'S';
+        case 'Fire': return 'F';
+        case 'Water': return 'W';
+        case 'Grass': return 'A';
+        case 'Electric': return 'E';
+        case 'Psychic': return 'C';
+        case 'Ice': return 'I';
+        case 'Dragon': return 'D';
+        case 'Dark': return 'K';
+        case 'Fairy': return 'X';
+        default: return '-';
+    }
+}
+
+main();
